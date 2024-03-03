7a31d7f83e2dd999d243a073d986bc2939cd6ad0
Abdelrhman Basyoni <64443028+abdelrhman-basyoni@users.noreply.github.com>
2024-03-03 22:51:29 +0200

    config

---
+++ src/config/db.config.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+  HOST: process.env.DATABASE_HOST,
+  USER: process.env.DB_USERNAME,
+  PASSWORD: process.env.DATABASE_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: "postgres",
+};
